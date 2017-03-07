4947180d58b3abd91918d617843ba51058cb45a4
yuanliang-IOS <yuanliangios@163.com>
2017-03-07 14:54:42 +0800

    splash启动屏幕设置

---
+++ whiteSP/android/app/src/main/java/com/whitesp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.whitesp;
+
+import com.facebook.react.ReactActivity;
+
+import android.os.Bundle;
+
+import com.cboy.rn.splashscreen.SplashScreen;
+
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "whiteSP";
+    }
+
+ @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this,true);
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
---
+++ whiteSP/index.android.js
@@ -0,0 +1,73 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import SplashScreen from 'react-native-splash-screen';
+
+
+export default class whiteSP extends Component {
+
+  componentDidMount() {
+    // do anything while splash screen keeps, use await to wait for an async task.
+
+    //数据加载操作（定位，网络请求）
+
+
+    //定时器关闭刷新
+    setTimeout(() => {SplashScreen.hide();}, 2500);
+
+
+
+
+  }
+
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('whiteSP', () => whiteSP);
